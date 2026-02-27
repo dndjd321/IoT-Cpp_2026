7b5322818d6ac19ca4c13ebe1091d61c0a67f2a2
ByeongJun Gu <rnqudwns2515@gmail.com>
2026-02-27 17:18:21 +0900

    Add files via upload

---
+++ base/File01.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+int main() {
+
+	int num = 20;
+	//printf("num : %d", num);
+
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello " << "World!" << std::endl;
+	std::cout << num << ' ' << 'A';
+	std::cout << ' ' << 3.14 << std::endl;
+
+	return 0;
+}
